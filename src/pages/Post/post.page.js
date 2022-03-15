4cf913650c83bbc5f159a5e5f5287712bb3baf4e
Rifat Islam <rifat.islam173@northsouth.edu>
2022-03-15 20:52:50 +0600

    migrated post-page with redux

--- src/pages/Post/post.page.js
+++ src/pages/Post/post.page.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
 import { useLocation } from 'react-router-dom'
 import Post from '../../components/Post/post.component'
 import './post-page.styles.css'
 import Comment from "../../components/Comment/comment.component";
+import { useGetCommentsQuery } from "../../services/posts"
 
 function PostPage() {
     const { state } = useLocation();
     const { post } = state;
+    const { data, isLoading, isSuccess } = useGetCommentsQuery(post.id)
 
-    
     return (
         <div className="post-container">
             <Post post={post} />
             <h3>Comments</h3>
             <div className="comments-container">
-                {!isLoaded && <h2> Loading.... </h2>}
+                {isLoading && <h2> Loading.... </h2>}
 
-                {comments.map((comms, index) =>
+                {isSuccess && data.map((comms, index) =>
                     <Comment comment={comms} key={index} />)}
             </div>
         </div >
